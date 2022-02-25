52937d6974cb81d39189e2b81870ff2085beeee9
shital kisan janjire <shitaljanjire27@gmail.com>
2022-02-25 11:18:03 +0530

    added AuthenticationAnAuthorization

---
+++ AuthenticationAndAuthoriztion/AuthenticationAndAuthoriztion/Authentication/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationAndAuthoriztion.Authentication
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="UserName is Required")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage ="Password is requitred")]
+        public string Password { get; set; }
+    }
+}
---
+++ AuthenticationAndAuthoriztion/AuthenticationAndAuthoriztion/Authentication/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationAndAuthoriztion.Authentication
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Phone number is required")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
